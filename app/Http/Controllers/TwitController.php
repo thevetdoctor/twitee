d03a508d5d674b4cb9016a58e34bc731c4c452be
obafemi oderanti <thevetdoctor@gmail.com>
2021-11-28 23:37:23 +0100

    added initial controller-route logic

---
+++ app/Http/Controllers/TwitController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TwitController extends Controller
+{
+    //
+    function getTwit()
+    {
+        return [
+            'name' => 'obafemi', 
+            'email' => 'thevetdoctor@gmail.com', 
+            'username' => 'oba',
+            'dateCreated' => '2021-11-28'
+        ];
+    }
+}
